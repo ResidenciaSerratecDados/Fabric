46a8f208bf293fad07ea5d5c25ff332ec446f8d1
MURILO409 <murilodasdoresalves@gmail.com>
2025-06-25 21:44:11 -0300

    SQL no Fabric

---
+++ Categoria_Produto.sql
@@ -0,0 +1,15 @@
+SELECT * FROM dbo.categoria;
+
+'selecionar produtos com preços acima da média.'
+SELECT * FROM dbo.produto WHERE VALOR_UNITARIO > (SELECT AVG(VALOR_UNITARIO) FROM dbo.produto);
+
+'inner join produto categoria'
+SELECT
+    p.NOME AS produto,
+    p.CUSTO_MEDIO AS custo_medio,
+    p.VALOR_UNITARIO AS valor_unitario,
+    c.NOME AS categoria
+FROM 
+    produto p
+INNER JOIN 
+    categoria c ON p.ID_CATEGORIA = c.IDCATEGORIA;
